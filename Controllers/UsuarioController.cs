4b49173889a7e37ec54233aafbbeec7ad903a669
joao <joao.vitor96silva@gmail.com>
2021-08-04 23:19:32 -0300

    Inserçao de arquivos
    arquivos para autenticaçao do login

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AspCore_AutenticaCookie.Controllers
+{
+    [Authorize]
+    public class UsuarioController : Controller
+    {
+       public IActionResult UsuarioHome()
+       {
+          return View();
+       }
+
+       [HttpGet]
+       public async Task<IActionResult> Logout()
+       {
+           await HttpContext.SignOutAsync();
+           return RedirectToAction("LoginUsuario", "Login");
+       }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,59 @@
+﻿using AspNetCoreLoginWithCookie.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreLoginWithCookie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IAutenticacao, Autenticacao>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(options =>
+                    {
+                        options.LoginPath = "/Login/LoginUsuario/";
+
+                    });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Login}/{action=LoginUsuario}/{id?}");
+            });
+        }
+    }
+}
